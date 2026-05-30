c2665e1744ec73a2a5cdf4efeaccfcb2ea39163a
bryanedmar <edbryan260@gmail.com>
2026-05-30 16:12:09 -0300

    Criação do perfil com infodados e uns bugs corrigidos

--- LocusFyapp/src/views/perfilView/styles.ts
+++ LocusFyapp/src/views/perfilView/styles.ts
@@ -24,4 +24,58 @@ export const styles = StyleSheet.create({
     gap: 10,
     backgroundColor: "#ffffff",
   },
+
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 30,
+  },
+
+  perfilContainer: {
+    marginBottom: 30,
+    elevation: 5, //pobroid
+    shadowColor: '#000', //IOS
+    shadowOffset: { width: 0, height: 2},
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+
+  fotodeperfil: {
+    width: 130,
+    height: 130,
+    borderRadius: 65, //parece que metade pra ficar bonito, n testei
+    backgroundColor: '#E0E0E0',
+    
+  },
+
+  infodadosContainer: {
+    width: '100%',
+  },
+  label: {
+    fontSize: 14,
+    color: '#555555',
+    fontWeight: '600',
+    marginBottom: 6,
+    marginTop: 12,
+  },
+  inputDisabled: {
+    backgroundColor: '#F5F5F5',
+    color: '#888888',
+    fontSize: 16,
+    padding: 12,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#E0E0E0',
+  },
+  infoBox: {
+    backgroundColor: '#FAFAFA',
+    color: '#333333',
+    fontSize: 16,
+    padding: 12,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#E0E0E0',
+    overflow: 'hidden',
+  },
 });
